18021e33768f8096d89c400e8c36542963bc2edf
Kevin Cauley <kacauley91@gmail.com>
2026-05-28 16:42:08 -0500

    Rebuild HR analytics portfolio site

--- build_site.R
+++ build_site.R
@@ -1,44 +1,25 @@
 library(rmarkdown)
 library(here)
 
-# Render Index (Home)
-rmarkdown::render(
-  input = here::here("analysis", "Index.Rmd"),
-  output_file = "index.html",
-  output_dir = here::here("docs")
-)
+message("Rendering portfolio site...")
 
-# Render EDA
-rmarkdown::render(
-  input = here::here("analysis", "HR_Data_EDA.Rmd"),
-  output_file = "HR_Data_EDA.html",
-  output_dir = here::here("docs")
-)
+render_report <- function(input_file, output_file) {
+  message("Rendering: ", input_file, " -> docs/", output_file)
+  
+  rmarkdown::render(
+    input = here::here("analysis", input_file),
+    output_file = output_file,
+    output_dir = here::here("docs"),
+    clean = TRUE,
+    envir = new.env(parent = globalenv())
+  )
+}
 
-# Render Turnover Intention
-rmarkdown::render(
-  input = here::here("analysis", "HR_Data_Turnover_Intention.Rmd"),
-  output_file = "HR_Data_Turnover_Intention.html",
-  output_dir = here::here("docs")
-)
+render_report("index.Rmd", "index.html")
+render_report("HR_Data_EDA.Rmd", "hr_data_eda.html")
+render_report("HR_Data_Turnover_Intention.Rmd", "hr_data_turnover_intention.html")
+render_report("HR_Data_Quiet_Quitting.Rmd", "hr_data_quiet_quitting.html")
+render_report("HR_Data_Monitoring_Privacy.Rmd", "hr_data_monitoring_privacy.html")
+render_report("HR_Data_Insights_Gleaned.Rmd", "hr_data_insights_gleaned.html")
 
-# Render Quiet Quitting
-rmarkdown::render(
-  input = here::here("analysis", "HR_Data_Quiet_Quitting.Rmd"),
-  output_file = "HR_Data_Quiet_Quitting.html",
-  output_dir = here::here("docs")
-)
-
-# Render Monitoring & Privacy
-rmarkdown::render(
-  input = here::here("analysis", "HR_Data_Monitoring_Privacy.Rmd"),
-  output_file = "HR_Data_Monitoring_Privacy.html",
-  output_dir = here::here("docs")
-)
-
-# Render Insights Gleaned
-rmarkdown::render(
-  input = here::here("analysis", "HR_Data_Insights_Gleaned.Rmd"),
-  output_file = "HR_Data_Insights_Gleaned.html",
-  output_dir = here::here("docs")
-)
+message("Done. HTML files were rendered to docs/.")
